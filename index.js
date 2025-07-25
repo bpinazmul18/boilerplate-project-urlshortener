2d796d53a47baf62c67088d02ecb38d14ee4fcc2
Nazmul Haque <bpinazmul18@gmail.com>
2025-07-25 10:40:25 +0600

    bring route in one file

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
 require("dotenv").config();
 const winston = require("winston");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+const { Url, validate } = require("./models/shorturl");
+
 require("./startup/logging")(app);
-require("./startup/routes")(app);
+// require("./startup/routes")(app);
 require("./startup/db")();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/shorturl", async (req, res) => {
+  // Validate input field
+  const { error, value } = validate({ original_url: req.body?.url });
+
+  if (error) return res.json({ error: "invalid url" });
+
+  // Find by url and return
+  // const url = await Url.findOne({ original_url: value.original_url });
+
+  // if (url) {
+  //   return res.json({
+  //     original_url: url.original_url,
+  //     short_url: url.short_url,
+  //   });
+  // }
+
+  const count = await Url.countDocuments({});
+
+  // New url
+  const newUrl = new Url({
+    original_url: value.original_url,
+    short_url: count + 1,
+  });
+
+  const _newUrl = await newUrl.save();
+
+  return res.json({
+    original_url: _newUrl.original_url,
+    short_url: _newUrl.short_url,
+  });
+});
+
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  const short_url = req.params.short_url;
+
+  try {
+    const url = await Url.findOne({ short_url });
+
+    if (!url) {
+      return res.status(404).json({ error: "URL was not found" });
+    }
+
+    return res.redirect(url.original_url);
+  } catch (err) {
+    winston.error("Error retrieving URL:", err);
+    return res.status(500).json({ error: "Error retrieving URL" });
+  }
+});
 
 const port = process.env.PORT || 3000;
 
--- routes/shorturl.js
+++ routes/shorturl.js
@@ -22,27 +22,21 @@ router.post("/", async (req, res) => {
 
   const count = await Url.countDocuments({});
 
-  console.log({
-    original_url: value.original_url,
-    short_url: count + 1,
-  });
-
   // New url
   const newUrl = new Url({
     original_url: value.original_url,
     short_url: count + 1,
   });
 
-  await newUrl.save();
+  const _newUrl = await newUrl.save();
 
   return res.json({
-    original_url: newUrl.original_url,
-    short_url: newUrl.short_url,
+    original_url: _newUrl.original_url,
+    short_url: _newUrl.short_url,
   });
 });
 
 router.get("/:id", async (req, res) => {
-  console.log("me");
   const shortUrl = req.params.id;
 
   try {
